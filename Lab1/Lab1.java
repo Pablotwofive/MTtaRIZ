b8f177f2bf7aceecfc2ed8e71aaeee7833fbd29c
Pavlo <pablito2005@PCX3D.localdomain>
2025-09-30 13:51:12 +0300

    ЛР1: добавлен второй вывод

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,5 +1,6 @@
 class Lab1 {
     public static void main(String[] args) {
         System.out.println("Its works");
+        System.out.println("Lab1 completed"); // <- добавили новую строку
     }
 }
