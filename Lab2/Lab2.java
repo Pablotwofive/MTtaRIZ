387f3881a73ea595c37c45803bd45892ea74b0d5
Pablotwofive <goofpablo@gmail.com>
2025-10-14 08:38:54 +0300

    Обнова

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,19 +1,5 @@
 public class Lab2 {
 
-    // Метод для вывода ёлки
-    public static void printTree(int levels) {
-        for (int i = 0; i < levels; i++) {
-            for (int j = 0; j < levels - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < (2 * i + 1); j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    // Метод для вывода 2D массива
     public static void printArray(int rows, int cols) {
         int[][] arr = new int[rows][cols];
         int value = 1;
@@ -29,12 +15,14 @@ public class Lab2 {
     }
 
     public static void main(String[] args) {
-        // Вызов метода ёлки
-        System.out.println("Ялинка:");
-        printTree(10);
+        Tree tree = new Tree(6);
 
-        // Вызов метода массива
+        System.out.println("Ялинка:");
+        tree.printTree();
+
         System.out.println("\nМасив:");
         printArray(3, 4);
     }
 }
+
+
