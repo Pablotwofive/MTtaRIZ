9888c9569db5a8ddca91199f4275577b108a62b7
Pablotwofive <goofpablo@gmail.com>
2025-10-14 08:30:04 +0300

    Апдейт

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,8 +1,6 @@
 class Lab1 {
     public static void main(String[] args) {
         System.out.println("Its works");
-        System.out.println("Lab1 completed"); // <- добавили новую строку 
-        System.out.println("Lab1 completed"); // <- добавили новую строку 
-        System.out.println("Lab1 completed"); // <- добавили новую строку 
+        System.out.println("Lab1 completed"); // <- добавили новую строку   
     }
 }
