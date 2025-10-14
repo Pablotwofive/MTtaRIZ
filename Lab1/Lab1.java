8a9cffb7f3f1dead75ccc38bba6ff20d7a11cad2
Pablotwofive <goofpablo@gmail.com>
2025-10-14 09:03:36 +0300

    Обнова

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,7 +1,5 @@
 class Lab1 {
     public static void main(String[] args) {
-        System.out.println("Its works");
-        System.out.println("Lab1 completed"); // <- добавили новую строку 
-        System.out.println("Lab1 completed"); // <- добавили новую строку     
+        System.out.println("Its works");        
     }
 }
